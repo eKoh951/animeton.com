56d7b4b97f382adaa470d0abb1f92f2bc13c557e
eKoh951 <greatekoh@gmail.com>
2024-10-17 16:17:33 -0600

    fix sites

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,9 +2,9 @@ export const siteConfig = {
   name: "Animeton",
   description: "La App de Windows para ver anime en Español",
   links: {
-    discord: "https://discord.gg/animeton",
-    instagram: "https://instagram.com/animeton",
-    tiktok: "https://tiktok.com/@animeton",
-    youtube: "https://youtube.com/@animeton",
+    discord: "https://discord.gg/fYNNmKJJfk",
+    instagram: "https://instagram.com/animeton_la",
+    tiktok: "https://tiktok.com/@animeton_la",
+    youtube: "https://youtube.com/@animeton_la",
   },
 };
