7518ccb1c7a162e25b805167fb69b613dea5142e
eKoh951 <greatekoh@gmail.com>
2024-10-18 23:03:34 -0600

    added changelog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import DotPattern from "@/components/ui/dot-pattern";
 import { HeroSection } from "./components/HeroSection";
+import { Changelog } from "@/components/ui/Changelog";
+import { getChangelog } from "@/lib/getChangelog";
 
 export default async function Home() {
+  const changelog = await getChangelog();
+
   return (
     <>
       <div className="flex items-center justify-center max-w-6xl mx-auto px-4 md:px-6 h-[90vh]">
         <HeroSection />
         <DotPattern className="[mask-image:radial-gradient(400px_circle_at_center,white,transparent)] color-primary-400" />
       </div>
+      {changelog && <Changelog releases={changelog} />}
     </>
   );
 }
