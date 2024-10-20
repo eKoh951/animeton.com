72d327ffc9c43ea31ba3386879e505bcb74dd670
eKoh951 <greatekoh@gmail.com>
2024-10-20 00:32:57 -0600

    installed posthog

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,25 @@
 // app/providers.tsx
 "use client";
 
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 import { NextUIProvider } from "@nextui-org/react";
 
+if (typeof window !== "undefined") {
+  if (!process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+    throw new Error("NEXT_PUBLIC_POSTHOG_KEY is not set");
+  }
+
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+  });
+}
+
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <NextUIProvider>
+      <PostHogProvider client={posthog}>{children}</PostHogProvider>
+    </NextUIProvider>
+  );
 }
