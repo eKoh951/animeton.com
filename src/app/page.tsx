050ca567f96e835f67c4676c318aa3a63f8ff088
eKoh951 <greatekoh@gmail.com>
2024-10-21 22:31:29 -0600

    fixed header and aligned hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
 
   return (
     <>
-      <div className="flex items-center justify-center max-w-6xl mx-auto px-4 md:px-6 h-[90vh]">
+      <div className="flex items-center justify-center max-w-6xl mx-auto px-4 md:px-6 h-full py-6 md:py-0 md:h-[90vh]">
         <HeroSection />
       </div>
       {changelog && <Changelog releases={changelog} />}
